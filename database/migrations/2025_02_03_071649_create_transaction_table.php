248ef760cee8231d8ba937694f802947766a7a7c
danu182 <danu182.182@gmail.com>
2025-02-03 14:32:29 +0700

    add migration table

---
+++ database/migrations/2025_02_03_071649_create_transaction_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('appointment_id');
+            $table->string('fee_doctor');
+            $table->string('fee_specialist');
+            $table->string('fee_hospital');
+            $table->string('subtotal');
+            $table->string('vet');
+            $table->string('total');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
