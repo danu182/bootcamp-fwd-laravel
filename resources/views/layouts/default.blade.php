f212a0f06d66e159e383004434dc6bc69de6a6d6
danu182 <danu182.182@gmail.com>
2025-02-11 11:44:38 +0700

    update all for slicing

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+    {{-- include meta star --}}
+    @include('include.backsite.meta')
+    {{-- include meta end --}}
+    <title>@yield('title') | MeetDocters</title>
+    
+
+    @stack('before-style')
+        {{-- include css link star --}}
+        @include('include.frontsite.style')
+        {{-- include css link end --}}
+    @stack('after-style')
+
+    </head>
+        <body>
+            {{-- include header start --}}
+            @include('components.frontsite.header')
+            {{-- include header end --}}
+
+
+            {{-- content start --}}
+            @yield('content')
+            {{-- content end --}}
+
+            {{-- include footer start --}}
+            @include('components.frontsite.footer')
+            {{-- include footer end --}}
+
+            @stack('before-script')
+                {{-- include script start --}}
+                @include('include.frontsite.script')
+                {{-- include script end --}}
+            @stack('after-script')
+
+            {{-- modal start --}}
+            {{-- ketik disini --}}
+            {{-- modal end --}}
+
+        </body>
+</html>
